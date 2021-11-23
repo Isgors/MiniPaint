a15cbac546c2fc213633f913e80133991aceb7d4
Igor de Souza <igor.carlosouza@gmail.com>
2021-11-23 11:47:34 -0300

    Added onSizeChanged

--- app/src/main/java/com/igordesouza/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/igordesouza/minipaint/MyCanvasView.kt
@@ -1,7 +1,27 @@
 package com.igordesouza.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.ContextCompat
 
-class MyCanvasView(context : Context) : View(context){
+class MyCanvasView(context : Context) : View(context) {
+
+    private lateinit var cacheCanvas: Canvas
+    private lateinit var cacheBitmap: Bitmap
+
+
+    private val backgroundColor = ContextCompat.getColor(context, R.color.colorBackground)
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        if(::cacheBitmap.isInitialized)
+            cacheBitmap.recycle()
+
+        cacheBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        cacheCanvas = Canvas(cacheBitmap)
+        cacheCanvas.drawColor(backgroundColor)
+    }
 }
