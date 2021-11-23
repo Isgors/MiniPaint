307571b7f4d00435b6afbd01210fddaedc171ed7
Igor de Souza <igor.carlosouza@gmail.com>
2021-11-23 11:38:26 -0300

    Display view on screen

--- app/src/main/java/com/igordesouza/minipaint/MainActivity.kt
+++ app/src/main/java/com/igordesouza/minipaint/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.igordesouza.minipaint
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.view.WindowInsetsController
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val myCanvasView = MyCanvasView(this)
+        setViewVisibilityFullscreen(myCanvasView)
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
+    }
+
+    private fun setViewVisibilityFullscreen(myCanvasView: MyCanvasView) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val controller = myCanvasView.windowInsetsController
+            controller?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            controller?.hide(WindowInsetsCompat.Type.systemBars())
+        } else {
+            myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        }
     }
 }
---
+++ app/src/main/java/com/igordesouza/minipaint/MyCanvasView.kt
@@ -0,0 +1,7 @@
+package com.igordesouza.minipaint
+
+import android.content.Context
+import android.view.View
+
+class MyCanvasView(context : Context) : View(context){
+}
